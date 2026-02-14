f6b5172ba1d2f644d4b87bc6677c953a52a709c4
Léa <lea.ballester@epitech.eu>
2026-02-14 11:35:00 +0100

    [SKILLS] Ajout des compétences

--- src/app/components/SectionSkills.tsx
+++ src/app/components/SectionSkills.tsx
@@ -1,7 +1,59 @@
-export default function Skills() {
+export default function SectionSkills() {
   return (
     <section>
-      <p>Compétences</p>
+      <h2>Mes Compétences</h2>
+
+      {/* BLOC 1 : DÉVELOPPEMENT WEB */}
+      <div>
+        <h3>Développement Frontend</h3>
+        <ul>
+          <li>JavaScript / TypeScript</li>
+          <li>React / Next.js / Angular / Vue.js</li>
+          <li>HTML5 / CSS3 / Sass / Tailwind CSS / Material UI</li>
+        </ul>
+      </div>
+
+      <div>
+        <h3>Développement Backend</h3>
+        <ul>
+          <li>Node.js (Express)</li>
+          <li>PHP (Natif / Laravel / Symfony)</li>
+          <li>Java (Spring)</li>
+          <li>MySQL / MongoDB / PostgreSQL</li>
+        </ul>
+      </div>
+
+      {/* BLOC 2 : INFRASTRUCTURE & SÉCURITÉ */}
+      <div>
+        <h3>Infrastructure, Sécurité & Cloud</h3>
+        <ul>
+          <li>Docker & Docker Compose</li>
+          <li>VPS OVH / Hostinger / Plesk</li>
+          <li>Cloudflare (DNS, Proxy, Security)</li>
+          <li>Sécurité WP (Wordfence) & SMTP (Mandrill, Mailchimp)</li>
+        </ul>
+      </div>
+
+      {/* BLOC 3 : CMS & SOLUTIONS LOW-CODE*/}
+      <div>
+        <h3>CMS & Écosystème WordPress</h3>
+        <ul>
+          <li>WordPress (Expertise Elementor & ACF)</li>
+          <li>Développement de thèmes & plugins</li>
+          <li>Solutions E-mailing & Marketing Automation</li>
+        </ul>
+      </div>
+
+      {/* BLOC 4 : OUTILS & OS */}
+      <div>
+        <h3>Environnement & Worklow</h3>
+        <ul>
+          <li>Git / GitHub / Github Desktop</li>
+          <li>Postman / Insomnia / Jira / Trello / Notion</li>
+          <li>Linux (Ubuntu) / Windows / MacOS</li>
+          <li>Design : Figma / Canva / Photopea</li>
+        </ul>
+      </div>
     </section>
   );
 }
